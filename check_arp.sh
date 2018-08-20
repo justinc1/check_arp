a12f3a805086fd8acb27efe6974863cadb1b2a50
Justin Cinkelj <justin.cinkelj@xlab.si>
2018-08-20 14:57:43 +0200

    Add EXPECTED_MAC option
    And fix typo in getopts usage.
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- check_arp.sh
+++ check_arp.sh
@@ -4,8 +4,9 @@
 # https://exchange.nagios.org/directory/Plugins/System-Metrics/Networking/check_arp/details
 
 ETH_DEV="eth0"
+EXPECTED_MAC=""
 
-while getopts "H:" OPTION;
+while getopts "H:I:M:" OPTION;
 do
   case $OPTION in
     "H") # Assign hostname
@@ -14,8 +15,15 @@ do
     "I") # Assign hostname
       ETH_DEV="$OPTARG"
     ;;
+    "M") # Assign expected MAC address
+      EXPECTED_MAC="$OPTARG"
+    ;;
   esac
 done
+
+# convert MAC to all uppercase, only ':' separated string
+# expect only : and - as separator.
+EXPECTED_MAC=$(echo "$EXPECTED_MAC" | sed -e 's/-/:/g' | tr '[:lower:]' '[:upper:]')
 
 if [ -z $HOST_NAME ]; then
   # we need this parameter to continue
@@ -31,8 +39,18 @@ else
       EXIT_STRING=$(echo -e "ERROR - multiple responses\n$EXIT_STRING")
       EXIT_CODE=2
     else
-      EXIT_STRING=$(echo -e "OK - single response\n$EXIT_STRING")
-      EXIT_CODE=0
+      if [ -z "$EXPECTED_MAC" ]; then
+        EXIT_STRING=$(echo -e "OK - single response\n$EXIT_STRING")
+        EXIT_CODE=0
+      else
+        if [ -z "$(echo "$EXIT_STRING" | grep "$EXPECTED_MAC" )" ]; then
+          EXIT_STRING=$(echo -e "ERROR - expected MAC $EXPECTED_MAC not found\n$EXIT_STRING")
+          EXIT_CODE=2
+        else
+          EXIT_STRING=$(echo -e "OK - expected MAC $EXPECTED_MAC present\n$EXIT_STRING")
+          EXIT_CODE=0
+        fi
+      fi
     fi
   else
     EXIT_STRING="WARNING: Unreachable host $HOST_NAME via $ETH_DEV\n"
